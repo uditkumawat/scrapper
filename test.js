28e794407bf61c12281c3ab3bf619ed6562e20dd
cl-macmini-122 <cl-macmini-122@cl-macmini-122.local>
2017-05-29 17:07:16 +0530

    added init method in test.js

--- test.js
+++ test.js
@@ -4,12 +4,45 @@ const request = require('request');
 const fs = require('fs');
 const urlRegex = require('url-regex');
 
-let urls = ['https://medium.com'];
+let urls = [];
 
 let url = null;
 
-let totalURLToTraverse = 10000000;                 //setting a limit to traverse (depth)
-let indexingURL=0;
+let totalURLToTraverse = 1000;                 //setting a limit to traverse (depth)
+let indexingURL = 0;
+
+function init(callback){
+
+        request('https://www.medium.com', function (error, response, body) {
+
+            if(error)
+                callback(error);
+            else if(response && response.statusCode==200) {
+
+                urls = body.match(urlRegex());
+
+                //asynchronously add these urls in csv file
+                fs.appendFile('urls.csv',urls,function(){
+
+                });
+
+                callback(null,urls);
+            }
+        });
+}
+
+init(function(error,data){
+    
+    if(error)
+        console.log("Error in request module");
+    else if(data.length>0)
+    {
+        // 5 concurrent request
+        for (let i = 0; i < 5; i++) {
+            getUrl();
+        }
+    }
+});
 
 function getUrl(){
 
@@ -17,14 +50,13 @@ function getUrl(){
     
     if(indexingURL>totalURLToTraverse)
         return;
-    
+
     //remove first element (URL) from array and store in variable for further processing
-    
+
     url = urls.shift();
-
-
+    
     if(url!=undefined && url!=null) {
-     
+
         request(url, function (error, response, body) {
 
             if (response && response.statusCode == 200) {
@@ -36,6 +68,7 @@ function getUrl(){
                 urls = [...new Set(urls)];
 
                 //asynchronously add these urls in csv file
+
                 fs.appendFile('urls.csv', urlsFromThisPage, function () {
 
                 });
@@ -44,11 +77,8 @@ function getUrl(){
             }
         });
     }
-}
-
-// 5 concurrent request
-
-for(let i=0;i<5;i++)
-{
-    getUrl();
+    else{
+        
+        getUrl();
+    }
 }
